f065bb51d8bd2a70224cbd30effd462dff4afb87
Saroj Debata <sdebata@CIBER-INDIA.IN>
2021-02-12 14:11:10 +0530

    updated code as per mysql

--- db-service/src/main/java/com/sd/spring/dbservice/model/Quotes.java
+++ db-service/src/main/java/com/sd/spring/dbservice/model/Quotes.java
@@ -1,29 +1,19 @@
 package com.sd.spring.dbservice.model;
 
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Quotes {
 
-	public Quotes(String userName,List<String> quotes) {
-		this.userName=userName;
-		this.quotes=quotes;
-	}
-	
-	public Quotes() {}
-	
+
 	private String userName;
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public List<String> getQuotes() {
-		return quotes;
-	}
-	public void setQuotes(List<String> quotes) {
-		this.quotes = quotes;
-	}
 	private List<String> quotes;
 	
 }
